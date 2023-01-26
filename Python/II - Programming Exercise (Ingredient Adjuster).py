cb14996450562c51bb4903cf74ba38d00b9d9664
Kenneth Fugen <123564245+fudgeebohr@users.noreply.github.com>
2023-01-26 14:15:15 +0800

    Create II - Programming Exercise (Ingredient Adjuster).py

---
+++ Python/II - Programming Exercise (Ingredient Adjuster).py
@@ -0,0 +1,13 @@
+# inputs
+str_nmae = str(input("Enter your full name: "))
+num_cookies = int(input("How many cookies do you want to make? "))
+
+# calculations
+sugar = (1.5 / 48) * num_cookies
+butter = (1.0 / 48) * num_cookies
+flour = (2.75 / 48) * num_cookies
+
+# outputs
+print("You will need", sugar, "cups of sugar.")
+print("You will need", butter, "cups of butter.")
+print("You will need", flour, "cups of flour.")
